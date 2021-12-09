e0955f96d98ad17c0c40d09cedc6e82e4c617a4a
Vanessa <75372128+DI-Vanessa-B@users.noreply.github.com>
2021-12-09 23:07:02 +0100

    new test repo

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="container">
+  <h1>Welcome!</h1>
+</div>
+  
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  height: 100vh;
+  background: grey;
+  grid-template-columns: 2fr 1fr;
+  grid-template-rows: 1fr;
+}
+h1 {
+  font-size: 3vh;
+  color: aliceblue;
+}
+</style>
